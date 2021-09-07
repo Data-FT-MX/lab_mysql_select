fef091c83e527c309c4e38c3ac590acdcfbb0e4a
Saúl Oviedo <sauloviedoleon@gmail.com>
2021-09-07 11:21:47 -0400

    fix

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -55,8 +55,8 @@ GROUP BY au.au_id ORDER BY SUM(sa.qty) DESC;
 /* BONUS */
 
 SELECT
-	au.au_id AS 'AUTHOR ID',
-	au.au_lname AS 'LAST NAME',
+    au.au_id AS 'AUTHOR ID',
+    au.au_lname AS 'LAST NAME',
     au.au_fname AS 'FIRST NAME',
     SUM(sa.qty * ti.price * ti.royalty/100 * ta.royaltyper/100 + ti.advance * ta.royaltyper/100) as 'PROFIT'
 FROM authors au 
